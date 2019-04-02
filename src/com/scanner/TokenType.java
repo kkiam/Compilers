3b30b516e5404a7e9fb8a3b88e574c9be47e0285
kkiam <iam.kafimbwa@gmail.com>
2019-04-02 20:59:14 +0200

    edited comments

--- src/com/scanner/TokenType.java
+++ src/com/scanner/TokenType.java
@@ -1,17 +1,14 @@
 package com.scanner;
 
 public enum TokenType {
-    Unit,/*"Unit" identifier*/
-	_glob,/*"global" scope identifier*/
+    Unit,/*"Unit" keyword*/
+	_glob,/*"global" scope keyword*/
     _loc,/*"local" scope identifier*/
-    _init,
-    _declit,/**/
+    _init,/*Keyword for initializing variables*/
+    _var,/*keyword for declaring variables*/
     _const,
     _FIN,
     _Stop,/*instead of break in other languages*/
-    
-    
-    
     
     
     
