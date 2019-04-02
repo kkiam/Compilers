91f32168f5346d46dab1411ffafb20d273cc975a
kkiam <iam.kafimbwa@gmail.com>
2019-04-02 20:22:07 +0200

    TokenType enum created

---
+++ src/com/scanner/TokenType.java
@@ -0,0 +1,11 @@
+package com.scanner;
+
+public enum TokenType {
+    Unit,
+	_glob,/*program rule defined global Unit Token*/
+    _loc,/*program rule defined local Unit Token */
+    _init,
+    
+    
+    
+}
